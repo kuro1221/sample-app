f5a8e7e151dcbca6dff058d3db5fb0f220ac421a
kuro <toshras22@gmail.com>
2024-01-03 07:20:57 +0900

    Prettier導入

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import ReactDOM from "react-dom";
-import React from "react";
+import ReactDOM from 'react-dom'
+import React from 'react'
 
 export const App = () => {
-    const clickButton = () => {
-        alert("クリックテスト")
-    }
+  const clickButton = () => {
+    alert('クリックテスト')
+  }
 
-    const contentStyle = {
-        color: "red",
-        fontSize: "20px"
-    }
+  const contentStyle = {
+    color: 'red',
+    fontSize: '20px',
+  }
 
-    return(
-        <>
-            <p style={{ color: "red" }}>テスト</p>
-            <h1 style={{ contentStyle }}>こんにちは</h1>
-            <button onClick={clickButton} >ボタン</button>
-        </>
-    )
+  return (
+    <>
+      <p style={{ color: 'red' }}>テスト</p>
+      <h1 style={{ contentStyle }}>こんにちは</h1>
+      <button onClick={clickButton}>ボタン</button>
+    </>
+  )
 }
