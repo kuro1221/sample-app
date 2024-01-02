3daeb530eedb6107151358cd6faceb8f87db75c8
kuro <toshras22@gmail.com>
2024-01-03 06:34:58 +0900

    Remove under '/src' files and create App component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+export const App = () => {
+    return(
+        <>
+            <p>テスト</p>
+            <h1>こんにちは</h1>
+        </>
+    )
+}
