2f9ae1ecf1f9983e406caeb94556f819c202ed9d
kuro <toshras22@gmail.com>
2024-01-03 07:07:27 +0900

    Appコンポーネントにstyle追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import ReactDOM from "react-dom";
 import React from "react";
 
 export const App = () => {
+    const clickButton = () => {
+        alert("クリックテスト")
+    }
+
+    const contentStyle = {
+        color: "red",
+        fontSize: "20px"
+    }
+
     return(
         <>
-            <p>テスト</p>
-            <h1>こんにちは</h1>
+            <p style={{ color: "red" }}>テスト</p>
+            <h1 style={{ contentStyle }}>こんにちは</h1>
+            <button onClick={clickButton} >ボタン</button>
         </>
     )
 }
