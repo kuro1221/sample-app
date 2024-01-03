98843991244b0e08e251000f7872b8aa8e4b1292
kuro <toshras22@gmail.com>
2024-01-03 18:18:30 +0900

    useStateの練習

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom'
-import React from 'react'
+import { useState } from 'react'
 import { ColoredMessage } from './components/ColoredMessage'
 
 export const App = () => {
+  const [num, setNum] = useState(0)
+
   const clickButton = () => {
-    alert('クリックテスト')
+    setNum(num + 1)
   }
 
   const contentStyle = {
@@ -19,6 +21,7 @@ export const App = () => {
       <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
       <ColoredMessage color="blue">元気です！</ColoredMessage>
       <button onClick={clickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   )
 }
